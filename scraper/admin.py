bed1d74c47f49c86c1d248c1637e68eb8b35c2c9
Mehdi <abdimehdi1321@gmail.com>
2025-07-11 12:02:29 +0330

    feat: add scraper to admin

--- scraper/admin.py
+++ scraper/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Scraper
 
-# Register your models here.
+@admin.register(Scraper)
+class ScraperAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'created_at', 'updated_at')
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('name', 'email')
+    readonly_fields = ('created_at', 'updated_at')
+    
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('name', 'email', 'password')
+        }),
+        ('Timestamps', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',)
+        }),
+    )
+    
+    # Customize the admin list view
+    list_per_page = 25
+    ordering = ('-created_at',)
